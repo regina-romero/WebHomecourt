8dd16011545bd397de4ff1831d64719da08ad835
Cielo Maria Vega Godoy <a01741542@tec.mx>
2026-04-13 13:26:04 -0600

    Refactor MarcadorActivo component; simplify section structure

--- WebHomecourt/src/components/Home/Marcador.tsx
+++ WebHomecourt/src/components/Home/Marcador.tsx
@@ -76,7 +76,7 @@ function MarcadorActivo({juego}:MarcadorActivoProps){
   const segundos = useContadorTiempo(juego?.start_date);
 
     return(
-        <section className="px-4 md:px-14 py-5 bg-zinc-100 w-full">
+        <section>
           <article className="w-full px-5 py-7 bg-purple-900 rounded-2xl shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] outline outline-1 outline-offset-[-1px] outline-black/25 flex flex-col justify-start items-start gap-3.5 overflow-hidden">
             <header className="self-stretch flex justify-between items-center">
               <div className="flex justify-start items-center gap-4 md:gap-7">
