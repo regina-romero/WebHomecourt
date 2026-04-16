4c8ddc6df05a60b57f03df000c648357f935034d
Monica Guzman <a00838824@tec.mx>
2026-04-15 22:12:18 -0600

    Got rid de login warnings

--- WebHomecourt/src/pages/Login.tsx
+++ WebHomecourt/src/pages/Login.tsx
@@ -9,14 +9,14 @@ function Login() {
   const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState(''); // Future allow username
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  //const [error, setError] = useState('');
   const navigate = useNavigate();
-  const [user, setUser] = useState<any>(null);
+  //const [user, setUser] = useState<any>(null);
   const [errorMessage, setErrorMessage] = useState('');
 
   // Checks fields are filled in, and does the login w supabase 
   const handleLogin = async () => {
-    setError('');
+    //setError('');
     setErrorMessage('');
 
     // Checks fields are filled in
@@ -26,30 +26,22 @@ function Login() {
     } else if (email == '') {
       setErrorMessage("Please provide your email");
     } else if (password == '') {
-      setErrorMessage("Please provide your password")
+      setErrorMessage("Please provide your password");
     } else {
       // Filled everything in so tries to sign in now
       setLoading(true);
-      const { data, error } = await supabase.auth.signInWithPassword({ email, password });
+      const { error } = await supabase.auth.signInWithPassword({ email, password });
       if (error) {
-        setError(error.message);
-        console.log(`Error iniciando sesión ${error.message}`);
+        //setError(error.message);
+        console.log(`Error iniciando sesión ${error}`);
         setErrorMessage("Incorrect credentials");
       } else {
         console.log(`Sí inició sesión`);
-        setUser(data.user); // Sets the user data 
+        //setUser(data.user); // Sets the user data 
         navigate('/');
       }
     }
   };
-
-  // Sends request to reset password
-  /*
-  const handleForgotten = async () => {
-    const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
-      redirectTo: 'https://example.com/update-password',
-    })
-  }*/
 
   return (
     // Uses relative so that it calcs diff layout positions to this main div */}
